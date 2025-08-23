ff7a009a18f18046f6ad0f05338b682a37d10bc9
LilekK <peeterkraus123@gmail.com>
2025-08-23 11:50:47 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,3 @@
+// Set current year in footer
+document.getElementById('y').textContent = new Date().getFullYear();
+// Lazy-load images (if you add loading="lazy" in HTML, browsers do it; this is extra)
